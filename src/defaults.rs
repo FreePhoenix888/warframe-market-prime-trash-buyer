028c93798f98a8704bbc7e66ffd6d5a4f94d4809
FreePhoenix888 <freephoenix888@gmail.com>
2023-05-12 16:09:33 +0600

    Make get_message more readable

--- src/defaults.rs
+++ src/defaults.rs
@@ -9,8 +9,12 @@ pub fn filter(order: &crate::market::Order) -> bool {
 }
 
 pub fn get_message(order: &Order, get_sum: &Box<dyn Fn(&Order) -> i32>) -> String {
-    format!("/w {} Hi, {}! You have WTS order: {} for {} :platinum: for each on warframe.market. I will buy all {} pieces for {} :platinum: if you are interested :)",
-            order.user.ingame_name, order.user.ingame_name, order.item?.item_name, order.platinum, order.quantity, get_sum(&order))
+    let user_name = &order.user.ingame_name;
+    let item_name = &order.item?.item_name;
+    let platinum = order.platinum;
+    let quantity = order.quantity;
+    let sum = get_sum(&order);
+    format!("/w {user_name} Hi, {user_name}! You have WTS order: {item_name} for {platinum} :platinum: for each on warframe.market. I will buy all {quantity} pieces for {sum} :platinum: if you are interested :)")
 }
 
 pub fn get_sum(order: &Order) -> i32 {
