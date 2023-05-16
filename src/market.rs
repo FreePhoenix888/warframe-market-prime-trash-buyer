8b1e71fab7fc4001f5d63e394e855b56580c57f5
FreePhoenix888 <freephoenix888@gmail.com>
2023-05-16 21:13:12 +0600

    Add and use payload_bridge

--- src/market.rs
+++ src/market.rs
@@ -1,5 +1,8 @@
 use anyhow::Result;
+use reqwest::Response;
 use serde::{Deserialize, Serialize};
+use serde::de::DeserializeOwned;
+use serde_json::Value;
 
 const URL: &str = "https://api.warframe.market/v1";
 const ITEMS: &str = "https://api.warframe.market/v1/items";
@@ -8,20 +11,29 @@ pub struct Market {
     pub client: reqwest::Client,
 }
 
+async fn payload_bridge<T: DeserializeOwned>(res: Response, bridge: &str) -> Result<T> {
+    let bytes = res.bytes().await?;
+    let value: Value = serde_json::from_slice(&bytes)?;
+
+    Deserialize::deserialize(&value["payload"][bridge]).map_err(Into::into)
+}
+
 impl Market {
     pub fn new() -> Self {
         let client = reqwest::Client::new();
         Market { client }
     }
 
-    pub async fn fetch_items(&self) -> Result<ItemsApiResponse> {
-        Ok(self.client.get(ITEMS).send().await?.json().await?)
+    pub async fn fetch_items(&self) -> Result<Vec<Item>> {
+        let res = self.client.get(ITEMS).send().await?;
+        payload_bridge(res, "items").await
     }
 
 
-    pub async fn fetch_orders(&self, item_url: &str) -> Result<OrdersApiResponse> {
+    pub async fn fetch_orders(&self, item_url: &str) -> Result<Vec<Order>> {
         let url = format!("{ITEMS}/{item_url}/orders");
-        Ok(self.client.get(&url).send().await?.json().await?)
+        let res = self.client.get(&url).send().await?;
+        payload_bridge(res, "orders").await
     }
 }
 
